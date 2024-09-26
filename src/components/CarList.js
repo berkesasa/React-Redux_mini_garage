bd507b3e6bd8ce5050268ef6feaec588dc72d560
berkesasa <berkesasa11@gmail.com>
2024-09-26 21:16:03 +0300

    car list and form created, css updated

--- src/components/CarList.js
+++ src/components/CarList.js
@@ -1,8 +1,34 @@
+import { useDispatch, useSelector } from 'react-redux'
+
 import React from 'react'
+import { removeCar } from '../store'
 
 function CarList() {
+  const dispatch = useDispatch()
+  const cars = useSelector((state) => {
+    return state.cars.data
+  })
+
+  const handleCarDelete = (car) => {
+    dispatch(removeCar(car.id))
+  }
+
+  const renderedCars = cars.map((car) => {
+    return(
+      <div key={car.id} className='panel'>
+        <p>{car.name} - ${car.cost}</p>
+        <button
+        className='button is-danger'
+        onClick={() => handleCarDelete(car)}
+        >Delete</button>
+      </div>
+    )
+  })
   return (
-    <div>CarList</div>
+    <div className='car-list'>
+      {renderedCars}
+      <hr />
+    </div>
   )
 }
 
